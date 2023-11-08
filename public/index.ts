5f62dce11d2dbc5ddafbfd48c31d888c05d93d8c
QuocAnh189 <anhquoc18092003@gmail.com>
2023-11-08 15:25:25 +0700

    Initial commit

---
+++ public/index.ts
@@ -0,0 +1,5 @@
+import noavatar from "../public/noavatar.png";
+import astronaut from "../public/astronaut.png";
+import noproduct from "../public/noproduct.jpg";
+
+export { noavatar, astronaut, noproduct };
